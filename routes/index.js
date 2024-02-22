60f0ad88627481dcf89581532704dd32b99f447f
Jeffrey Craig Doyle <jeffreycraigdoyle@Jeffreys-Air.lan>
2024-02-22 11:05:48 -0500

    node server message board created

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date().getHours(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().getHours(),
+  },
+  {
+    text: "Who's this?",
+    user: "Dave",
+    added: new Date().getHours(),
+  },
+  {
+    text: "Peace",
+    user: "Zed",
+    added: new Date().getHours(),
+  },
+  {
+    text: "What up?",
+    user: "Dan",
+    added: new Date().getHours(),
+  },
+  {
+    text: "Heyyyy",
+    user: "Coryß",
+    added: new Date().getHours(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.post("/new", (req, res, next) => {
+  const { username, message } = req.body;
+
+  messages.push({
+    text: message,
+    user: username,
+    added: new Date().getHours(),
+  });
+
+  res.redirect("/");
+});
+
+module.exports = router;
